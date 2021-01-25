bdb779a6a80693c1243b1c6315b73ebde794e32e
Tristan0000 <tvancaspel@roc-dev.com>
2021-01-25 09:54:03 +0100

    h2 af

---
+++ H2/javascript.js
@@ -0,0 +1,8 @@
+
+
+
+
+document.getElementById("paginatitel").innerText = "pagina";
+document.getElementById("subkop").innerText = "subkop";
+document.getElementById("aap1").src = "img/aap1.jpg";
+document.getElementById("aap2").src = "img/aap2.jpg";
